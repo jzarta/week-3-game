fd5dc8b0d632387ccfb2378405cf34aa45e9575c
jzarta <jorge.zarta@knights.ucf.edu>
2016-04-21 18:03:01 -0400

    last

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,46 @@
+// Sets the computer choices 
+var computerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+                       'j', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 
+                       'u', 'v', 'w', 'x', 'y', 'z'];
 
-	
-// Sets the computer chooices 
-
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-//Declares the tallies to 0 to 9
+// Declares the tallies to 0 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-var yourGuessessofar = 0;
-var LettersGuessed = [];
+var guessesLeft = 0;
+var lettersLeft = [];
 
 // When the user presses the key it records the keypress and then sets it to userguess
-
 document.onkeyup = function(event) {
-	var userguess = String.fromCharCode(event.keyCode).toLowerCase();
-	var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-//picks a random letters between a and z
-	
-		function initGame(){
-	   
-	// User chooses a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z.
-	
-	
-	if (userguess == computerChoice){
-		wins++
-		} else {
-			losses++
-		}
-			guessesLeft--
-		LettersGuessed.push(userguess)
-		
+    // This sets the computer guess equal to the random.
+    var computerGuess = computerLetters[Math.floor(Math.random() * computerLetters.length)];
+    console.log (computerGuess)
 
-	// Taking the tallies and displaying them in HTML
-	var html = "<p>The Psychic Game</p>" +
-	"<p>wins: " + 
-	wins + 
-	"</p>" +
-	"<p>losses: " + 
-	losses + 
-	"</p>" +
-	guessesLeft
-	"<p>guessesLeft: " + 
-	yourGuessessofar
-	"<p>yourGuessessofar: " + 
-	"</p>"
-	LettersGuessed
-	"<p>LettersGuessed: " +
-	"</p>"
-	
-	initGame();
+     // It tests to determine if the computer or the user won the round and then increments 
+        if (userGuess == computerGuess){
+            wins++;
+        } else { 
+            losses++;
+        }
 
-	document.querySelector('#game').innerHTML = html
-	
-	}
+        //lettersLeft.push
 
-	
-}		
+        // Taking the tallies and displaying them in HTML
+        var html = "<p>Guess what letter I'm thinking of?</p>" +
+        "<p>wins: " + 
+        wins + 
+        "</p>" +
+        "<p>losses: " + 
+        losses + 
+        "</p>" +
+        "<p>guesses left: " + 
+        guessesLeft + 
+        "</p>" + 
+        "<p> letters left: " + lettersLeft + "</p>";
+
+        // Placing the html into the game ID
+     document.querySelector('#game').innerHTML = html;
+
+}	
 
